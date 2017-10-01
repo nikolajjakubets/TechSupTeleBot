8765d005f743c7be0e8eea4f59b090a71ef2bdf5
werwolf <13werwolf13@mail.ru>
2017-10-01 15:37:24 +0600

    new command

--- TehSupTeleBot.py
+++ TehSupTeleBot.py
@@ -3,8 +3,18 @@ import constants
 
 bot = telebot.TeleBot(constants.token)
 
+adminchatid = 0
+
 @bot.message_handler(commands=['help'])
 def handle_text(message):
     bot.send_message(message.chat.id, "Бот технической поддержки хостинга")
+
+@bot.message_handler(commands=['IAmAdmin'])
+def handle_text(message):
+    bot.send_message(message.chat.id, "Пользователь отмечен как администратор")
+    adminchatid = message.chat.id
+    bot.send_message(message.chat.id, adminchatid)
+
+
 
 bot.polling(none_stop=True, interval=0)
